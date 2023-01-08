4f1f0f0e86e21f3a17fb0adc519d7fdb99467fa8
Spycio.Kon <77606941+tiyeume25112004@users.noreply.github.com>
2023-01-08 12:47:47 +0700

    Create sqli_of_lord.php

---
+++ sqli_of_lord.php
@@ -0,0 +1,15 @@
+<?php 
+  include "./flag.php"; 
+  login_chk(); 
+  $db = dbconnect();
+  $_GET['id'] = strrev(addslashes($_GET['id']));
+  $_GET['pw'] = strrev(addslashes($_GET['pw']));
+  if(preg_match('/prob|_|\.|\(\)/i', $_GET[id])) exit("No Hack ~_~"); 
+  if(preg_match('/prob|_|\.|\(\)/i', $_GET[pw])) exit("No Hack ~_~"); 
+  $query = "select id from prob_zombie_assassin where id='{$_GET[id]}' and pw='{$_GET[pw]}'"; 
+  echo "<hr>query : <strong>{$query}</strong><hr><br>"; 
+  $result = @mysqli_fetch_array(mysqli_query($db,$query)); 
+  if($result['id']) solve("zombie_assassin"); 
+  highlight_file(__FILE__); 
+?>
+// hoiw khoai day :D
